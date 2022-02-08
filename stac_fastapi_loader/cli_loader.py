fe53dfede97e553b7544558f145505dfe87a971d
jonhealy1 <jonathan.d.healy@gmail.com>
2022-02-07 23:52:38 -0800

    flesh out load data

--- stac_fastapi_loader/cli_loader.py
+++ stac_fastapi_loader/cli_loader.py
@@ -1,5 +1,10 @@
 import click
+import os
+import json
 import requests
+
+DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
+STAC_API_BASE_URL = "http://localhost:8083"
 
 def cli_message():
     click.secho()
@@ -9,14 +14,25 @@ def cli_message():
 
     click.secho()
 
-def load_item():
+def load_data(filename):
+    with open(os.path.join(DATA_DIR, filename)) as file:
+        return json.load(file)
+
+def load_collection():
+    collection = load_data("test_collection.json")
     try:
-        r = requests.get("http://localhost:8083")
-        print(r.status_code)
-        print(r.content)
-        # prints the int of the status code. Find more at httpstatusrappers.com :)
+        resp = requests.post(f"{STAC_API_BASE_URL}/collections", json=collection)
+        click.secho(resp.status_code)
     except requests.ConnectionError:
-        print("failed to connect")
+        click.secho("failed to connect")
+
+def load_item(collection):
+    item = load_data("test_item.json")
+    try:
+        resp = requests.post(f"{STAC_API_BASE_URL}/collections/{collection}/items", json=collection)
+        click.secho(resp.status_code)
+    except requests.ConnectionError:
+        click.secho("failed to connect")
 
 # @click.option(
 #     "-l", "--links", is_flag=True, help="Validate links for format and response."
