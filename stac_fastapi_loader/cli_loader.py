b69715f4a065d4c836aefa6db649fbbc2877ff4d
jonhealy1 <jonathan.d.healy@gmail.com>
2022-02-07 23:18:48 -0800

    basic cli

--- stac_fastapi_loader/cli_loader.py
+++ stac_fastapi_loader/cli_loader.py
@@ -2,28 +2,18 @@ import click
 
 
 def cli_message():
-    click.secho("""
- ____  ____  __    ___       ___  _  _  ____  ___  __ _ 
-/ ___)(_  _)/ _\  / __)___  / __)/ )( \(  __)/ __)(  / )
-\___ \  )( /    \( (__(___)( (__ ) __ ( ) _)( (__  )  ( 
-(____/ (__)\_/\_/ \___)     \___)\_)(_/(____)\___)(__\_)
-    """)
+    click.secho()
+    click.secho("""STAC FASTAPI LOADER""")
 
-    click.secho("stac-check: STAC spec validaton and linting tool", bold=True)
+    click.secho("stac-fastapi-loader: Load STAC data into fastapi", bold=True)
 
     click.secho()
 
-@click.option(
-    "-r", "--recursive", is_flag=True, help="Validate all assets in a collection or catalog."
-)
-@click.option(
-    "-a", "--assets", is_flag=True, help="Validate assets for format and response."
-)
-@click.option(
-    "-l", "--links", is_flag=True, help="Validate links for format and response."
-)
+# @click.option(
+#     "-l", "--links", is_flag=True, help="Validate links for format and response."
+# )
 @click.command()
-@click.argument('file')
+@click.argument('backend')
 @click.version_option(version="0.1.4")
-def main(file, assets, links, recursive):
+def main(backend):
     cli_message()
