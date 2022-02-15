e0e74bf4ab98ba787ab62c24b91c8b8f927a7b7c
jonhealy1 <jonathan.d.healy@gmail.com>
2022-02-15 03:47:29 -0800

    test 10000 points

--- tests/mongo/test_point_query.py
+++ tests/mongo/test_point_query.py
@@ -1,9 +1,16 @@
 import requests
 import pytest 
+from random import randint
 
 STAC_API_BASE_URL = "http://localhost:8083"
 
 COORDINATES = [[175.8, -82.84], [176.8, -82.84]]
+
+for x in range(10000):
+    coord_1 = randint(-180, 180)
+    coord_2 = randint(-90, 90)
+    coordinates = [coord_1, coord_2]
+    COORDINATES.append(coordinates)
 
 @pytest.mark.parametrize('coordinates', COORDINATES)
 def test_point_query(coordinates): 
@@ -12,5 +19,6 @@ def test_point_query(coordinates):
         "intersects":{"type": "Point", "coordinates": coordinates}
     }
     resp = requests.post(f"{STAC_API_BASE_URL}/search", json=body)
-    data = resp.json()
-    assert data["context"]["returned"] >= 10
+    assert resp.status_code == 200
+    # data = resp.json()
+    # assert data["context"]["returned"] >= 10
