a3183ec14b312eb0b0950c2ccfbefea5d3c1fcf8
jonhealy1 <jonathan.d.healy@gmail.com>
2022-02-15 03:20:50 -0800

    paramertize coordinates

--- tests/mongo/test_point_query.py
+++ tests/mongo/test_point_query.py
@@ -1,11 +1,15 @@
 import requests
+import pytest 
 
 STAC_API_BASE_URL = "http://localhost:8083"
 
-def test_point_query():
+COORDINATES = [[175.8, -82.84], [176.8, -82.84]]
+
+@pytest.mark.parametrize('coordinates', COORDINATES)
+def test_point_query(coordinates): 
     body = {
         "collections":["sentinel-s2-l2a-cogs"],
-        "intersects":{"type": "Point", "coordinates": [175.8, -82.84]}
+        "intersects":{"type": "Point", "coordinates": coordinates}
     }
     resp = requests.post(f"{STAC_API_BASE_URL}/search", json=body)
     data = resp.json()
