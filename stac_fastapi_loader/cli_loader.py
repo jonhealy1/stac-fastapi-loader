70f064b96fde14309d4f536070b7d8902779c59d
jonhealy1 <jonathan.d.healy@gmail.com>
2022-02-14 23:28:58 -0800

    load all data files in folder

--- stac_fastapi_loader/cli_loader.py
+++ stac_fastapi_loader/cli_loader.py
@@ -3,7 +3,7 @@ import os
 import json
 import requests
 
-DATA_DIR = os.path.join(os.path.dirname(__file__), "test_data")
+DATA_DIR = os.path.join(os.path.dirname(__file__), "test_data/sentinel_data")
 STAC_API_BASE_URL = "http://localhost:8083"
 
 def cli_message():
@@ -19,26 +19,34 @@ def load_data(filename):
         return json.load(file)
 
 def load_collection(collection_id):
-    collection = load_data("test_collection.json")
+    data_dir = os.path.join(os.path.dirname(__file__), "test_data")
+    with open(os.path.join(data_dir, "test_collection.json")) as file:
+        collection = json.load(file)
     collection["id"] = collection_id
     try:
         resp = requests.post(f"{STAC_API_BASE_URL}/collections", json=collection)
-        # click.secho(resp.status_code)
     except requests.ConnectionError:
         click.secho("failed to connect")
 
 def load_item():
-    items = load_data("sentinel_data/sentinel-s2-l2a-cogs_100_10000.json")
-    collection = items["features"][0]["collection"]
+    feature_collections = []
+    # items = load_data("sentinel_data/sentinel-s2-l2a-cogs_100001_150000.json")
+    for filename in os.listdir(DATA_DIR):
+        item = load_data(filename)
+        feature_collections.append(item)
+        print(filename)
+
+    collection = item["features"][0]["collection"]
     load_collection(collection)
-    for feature in items["features"]:
-        try:
-            feature["stac_extensions"] = []
-            feature["stac_version"] = "1.0.0"
-            resp = requests.post(f"{STAC_API_BASE_URL}/collections/{collection}/items", json=feature)
-            # click.secho(resp.status_code)
-        except requests.ConnectionError:
-            click.secho("failed to connect")
+    
+    for items in feature_collections: 
+        for feature in items["features"]:
+            try:
+                feature["stac_extensions"] = []
+                feature["stac_version"] = "1.0.0"
+                resp = requests.post(f"{STAC_API_BASE_URL}/collections/{collection}/items", json=feature)
+            except requests.ConnectionError:
+                click.secho("failed to connect")
 
 # @click.option(
 #     "-l", "--links", is_flag=True, help="Validate links for format and response."
